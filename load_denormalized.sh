113b172e9dd8a7a80c27eb4e4c8866936057f22f
SybelF <sfrancois24@cmc.edu>
2023-04-12 15:19:50 -0700

    almost

--- load_denormalized.sh
+++ load_denormalized.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:3038/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+
+for file in $1; do
+    unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:3038/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+done
